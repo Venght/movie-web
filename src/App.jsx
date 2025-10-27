7b9e42e7537dd6d8684c5b4c3384c47b70c3de0d
Venght <bvenght@gmail.com>
2025-10-27 17:30:05 +0800

    fix the fetch error by moving the env.local which is the api into react-project folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,44 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import Search from './assets/search.jsx'
+
+const API_URL = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    authorization: `Bearer ${API_KEY}`
+  }
+}
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('')
+
+  const [errorMessage, setErrorMessage] = useState('')
+  
+  const fetchMovies = async () => {
+    try {
+      const endpoint = `${API_URL}/discover/movie?sort_by=popularity.desc`
+
+      const response = await fetch(endpoint, API_OPTIONS)
+
+      if(!response.ok) {
+        throw new Error('Failed to fetch the movie')
+      }
+      const data = await response.json();
+
+      console.log(data);
+    } catch (error) {
+      console.error('Error fetching movies:', error);
+      setErrorMessage('Failed to fetch movies. Please try again later.');
+    }
+  }
+  useEffect(() => {
+    console.log('useEffect ran');
+    fetchMovies();
+  }, []);
+
   return ( 
    <main>
     <div className = "pattern"></div>
@@ -11,11 +47,19 @@ const App = () => {
       <header>
         <h1>
           <img src = "hero.png" alt = "hero-banner"></img>
-          <span className="text-gradient">Find movies</span> here for free
-        </h1>
+          <span className="text-gradient">Find movies here for free </span>
+        </h1>      
+        
+        <Search searchTerm = {searchTerm} setSearchTerm = {setSearchTerm} />
+
       </header>
 
-      <Search searchTerm = {searchTerm} setSearchTerm = {setSearchTerm} />
+      <section className='all-movies'>
+        <h2>All movies</h2>
+
+        {errorMessage && <p className='text-red-500'>{errorMessage}</p>}
+      </section>
+
       <h1 className='text-white'>{searchTerm} </h1>
     </div> 
     </main>
